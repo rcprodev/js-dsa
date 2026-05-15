f46ca32b5d5b8c1dc05edc73325d692c7c18b393
rcprodev <113217696+rcprodev@users.noreply.github.com>
2026-05-15 10:39:52 +0530

    updated solution flattenArray

--- patterns/recursion/flattenArray.js
+++ patterns/recursion/flattenArray.js
@@ -1,1 +1,25 @@
 //Flatten a nested array
+
+// using recursion
+
+function flattenArray(arr) {
+  let result = [];
+
+  function flatten(item) {
+    for (let i = 0; i < item.length; i++) {
+      // If element is array
+      if (Array.isArray(item[i])) {
+        flatten(item[i]); // recursive call
+      } else {
+        result.push(item[i]);
+      }
+    }
+  }
+
+  flatten(arr);
+
+  return result;
+}
+
+console.log(flattenArray([1, [2, [3, 4]], 5]));
+// [1, 2, 3, 4, 5]
